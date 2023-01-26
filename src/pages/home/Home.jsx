ab3a7feffab2aef93175946d260ae8788905c0ba
Ronald George <ronaldsamgeorge@gmail.com>
2023-01-26 23:16:00 +0530

    Add Chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../dummyData";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        dataKey="Active User"
+        grid
+      />
     </div>
   );
 };
